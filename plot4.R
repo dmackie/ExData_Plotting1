9a475fc6e0f99decb7d7ca8c92972c00fe161e28
Dave Mackie <dave.mackie@gmail.com>
2016-06-01 22:38:58 +0200

    Removing accidentally typed letters

--- plot4.R
+++ plot4.R
@@ -48,7 +48,7 @@ with(hpc_filtered, {
     lines(Sub_metering_2 ~ datetime, type = "l", col = "red")
     lines(Sub_metering_3 ~ datetime, type = "l", col = "blue")
 })
-?png
+
 #Add plot legend
 legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        col = c("black", "red", "blue"), lty=c(1,1,1), bty = 'n'
